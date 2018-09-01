8e98fdb003592cd8d0c9b94b15592c51606bf4ce
varshavasthavi <vasthavip1998@gmail.com>
2018-09-01 20:35:34 +0530

    updated

---
+++ contol_flow_in_dart/logical.dart
@@ -0,0 +1,13 @@
+main(List<String> arguments){
+  //logical operators
+  int n=56;
+  int a=77;
+  if(n!=55){
+    print("it is not 55");
+}
+if(a>50 && n>50){
+  print("numbers are greater than 50");
+}
+if(a>50 || n>100){
+  print(" a is greater than 50 or n is greater than 100");
+}}
