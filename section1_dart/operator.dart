5e09b1a8b0a79805125ff2a19635d2189a0411d0
varshavasthavi <vasthavip1998@gmail.com>
2018-09-01 12:07:04 +0530

    operators and variables

---
+++ section1_dart/operator.dart
@@ -0,0 +1,15 @@
+main(List<string> arguments){
+  num num1=45;
+  num num2=5;
+  //arithmetic operators
+  print(num1+num2);
+  print(num1-num2);
+  print(num1*num2);
+  print(num1/num2);
+  print(num1%4);
+  //equality and relational operators
+  print(num1==num2);
+  print(num1!=num2);
+  print(num1>=num2);
+  print(num1<=num2);
+}
---
+++ section1_dart/variable.dart
@@ -0,0 +1,21 @@
+void main(){
+  var abc;
+  String s="string";
+  abc="country";
+  num age=22;
+  //int double and booleans
+  int i=55;
+  double d=5.66;
+  bool t=true;
+  const pi=3.14;//constant value at compile time
+  final p=8;//p will always be constant
+
+  print(abc);
+  print(s);
+  print(i);
+  print(d);
+  print(t);
+  print("the value of pi is: $pi");
+  print(p);
+  print("it's a ${abc.toUpperCase()}");
+}
